9803b1b9febf2766fa6730d3ae9474c38b3e2ada
germiiii <germi560@gmail.com>
2023-10-03 11:44:57 -0300

    first commit

---
+++ api/src/models/Pokemon.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('pokemon', {
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      unique: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    img:{
+      type: DataTypes.BLOB,
+      allowNull: false,
+    },
+    hp:{
+      type: DataTypes.INTEGER
+    }
+
+  });
+};
